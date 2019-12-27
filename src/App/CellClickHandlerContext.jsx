a442a330ffa42e89c4b5329ddca592516ea410a9
ganorberg <george.norberg@gmail.com>
2019-12-26 21:06:42 -0800

    Refactor and use context for cell click handler

---
+++ src/App/CellClickHandlerContext.jsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+const CellClickHandlerContext = createContext();
+CellClickHandlerContext.displayName = "CellClickHandlerContext";
+
+export default CellClickHandlerContext;
